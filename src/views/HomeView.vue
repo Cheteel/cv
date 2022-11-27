ba3d2d6ea3cbc23dc2a60bff25534244a75b89cc
cheteel <kl.reg@pm.me>
2022-11-27 17:05:29 +0100

    Cleanup done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,10 @@
+// HomeView
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <main></main>
 </template>
-
 <script lang="ts">
-import Vue from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
+import Vue from "vue";
 export default Vue.extend({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
+  name: "HomeView",
 });
 </script>
